d9a8573268cf019b4cda8c0ae613d7755577f8db
mn14tm <mn14tm@leeds.ac.uk>
2017-05-16 16:37:49 +0100

    tidy and added upconversion model
    Signed-off-by: mn14tm <mn14tm@leeds.ac.uk>

---
+++ Laser Rate Equations/upconversion/upconversion.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.integrate import odeint
+from scipy.optimize import curve_fit
+
+
+def decay_fn(t, a, tau, c):
+    """Single-exponential decay fluorescence function."""
+    return a * np.exp(-t / tau) + c
+
+
+def dndt(n, t, tau=10, C=3E-24, N=1.5E19):
+    """
+    Rate equation for population inversion when upconversion is present.
+    
+    :param n: fractional population inversion
+    :param t: 
+    :param tau: Spontaneous de-excitation rate in absence of migration-quenching (tau_rad + tau_nrad) [ms]
+    :param C: C_Er-Er [m3/s] for sodalime glass
+    :param N: Er concentration [at./cm3]
+    :return: dn/dt
+    """
+    return -n / tau - C * N * n ** 2
+
+
+if __name__ == "__main__":
+    # Material parameters
+    tau = 10
+    C = 3E-24
+    N = 1E21  # 1.5E19
+
+    # Time to simulate over
+    t = np.linspace(0, 1000, num=100)
+
+    # Starting population inversions
+    n_list = np.linspace(1E-3, 1, num=20)
+
+    tau_list = []
+    tau_std_list = []
+    for n in n_list:
+        # Evaluate ODE to get y data
+        y = odeint(dndt, y0=n, t=t, args=(tau, C, N))
+        y = y[:, 0]
+
+        # Fit a single exp. decay function using Levenberg-Marquardt algorithm
+        popt, pcov = curve_fit(decay_fn, t, y, p0=[max(y), 10, min(y)])
+
+        # Error in coefficients to 1 std.
+        perr = np.sqrt(np.diag(pcov))
+
+        tau_list.append(popt[1])
+        tau_std_list.append(perr[1])
+
+    fig, ax = plt.subplots()
+    # ax.plot(n_list, tau_list)
+    ax.errorbar(n_list, tau_list, yerr=tau_std_list)
+    ax.set_xlabel(r'n(t=0)')
+    ax.set_ylabel(r'tau (ms)')
+    ax.ticklabel_format(useOffset=False)
+    plt.show()
